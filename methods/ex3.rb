cce3960806a00604247f318352e45820c0ba32c9
serenahks <112879871+serenahks@users.noreply.github.com>
2022-09-24 13:59:35 -0400

    Create ex3.rb
    Write a program that includes a method called multiply that takes two arguments and returns the product of the two numbers.

---
+++ methods/ex3.rb
@@ -0,0 +1,12 @@
+#Write a program that includes a method called multiply that takes two arguments and returns the product of the two numbers.
+
+def multiply(number1, number2)
+    product = number1 * number2
+    puts "The two numbers are: ", number1, number2
+    puts "The product is: "
+    puts product
+end  
+
+a = 36
+b = 21
+multiply(a, b)
