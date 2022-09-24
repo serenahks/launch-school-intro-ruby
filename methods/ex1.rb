5c55a98b31f7e86548986666b2557fda629b9f17
serenahks <112879871+serenahks@users.noreply.github.com>
2022-09-24 13:21:06 -0400

    Create ex1.rb
    Solution to the first exercise in chapter methods of the course:
    Q. Write a program that prints a greeting message. 
    This program should contain a method called greeting that takes a name as its parameter and returns a string.

---
+++ methods/ex1.rb
@@ -0,0 +1,13 @@
+puts "What's your name?"
+name = gets
+
+def greeting (name)
+    puts 'Hello ' + name
+end
+
+greeting(name)
+
+
+def (greeting)
+     "Hello," + name + ". How are you doing?"
+end
