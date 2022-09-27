b3b838ca658e2e6f37fea264be0a7ce0ec58ec5e
serenahks <112879871+serenahks@users.noreply.github.com>
2022-09-26 23:37:03 -0400

    Create ex2.rb
    Write a method that takes a string as an argument. The method should return a new, all-caps version of the string, only if the string is longer than 10 characters. Example: change "hello world" to "HELLO WORLD". (Hint: Ruby's String class has a few methods that would be helpful. Check the Ruby Docs!)

---
+++ flow-control/ex2.rb
@@ -0,0 +1,10 @@
+def toUpperCase(helloworld)
+  if helloworld.length > 10
+    return helloworld.upcase
+  else 
+    return helloworld
+  end
+end 
+    
+puts toUpperCase("Hiiii whatsup")
+puts toUpperCase("hey")
